c952b10786d036dc4e96959f00db3cc430204de8
Pranik Chainani <pranikc@gmail.com>
2021-07-19 21:49:21 -0400

    commit tree c4ea9dd4989a0f9f8ea16e466b86cff20c99a6c1

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
@@ -14,7 +16,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(artist_params)
 
     if @song.save
-      redirect_to @song
+      redirect_to song_path(@song)
     else
       render :new
     end
